e741fb7a553fb9e041694e62c0c4421171d45936
Dnny <mcmillandanny@gmail.com>
2018-10-25 02:28:10 -0400

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 import './App.css';
 
-class App extends Component {
+class getData extends Component {
+  state = {
+    picture: [],
+    titles: []
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+     null    
+    )
   }
 }
 
